6d4b1c740de2bf9758dd26a6504dec3df848e513
Georgi Kurian <c0687988@mylambton.ca>
2017-01-20 16:57:41 -0500

    Made the class variables private

---
+++ src/csd4464/assignment2/phoenix/Student.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csd4464.assignment2.phoenix;
+
+/**
+ *
+ * @author c0687988
+ */
+public class Student {
+    private String name;
+    private String id;
+    private String gender;
+    private double grade;
+
+    public Student() {
+    }
+
+    public Student(String name, String id, String gender, double grade) {
+        this.name = name;
+        this.id = id;
+        this.gender = gender;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
