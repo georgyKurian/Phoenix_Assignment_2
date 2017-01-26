aade50d7266c5f5c3c4c7220236cacefeb4adae6
Georgi <Georgi@LAPTOP-0R3S8C76>
2017-01-25 19:38:47 -0500

    Changed project setting

--- src/csd4464/assignment2/phoenix/CSD4464Assignment1Phoenix.java
+++ src/csd4464/assignment2/phoenix/CSD4464Assignment1Phoenix.java
@@ -5,6 +5,9 @@
  */
 package csd4464.assignment2.phoenix;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author c0687988
@@ -15,7 +18,21 @@ public class CSD4464Assignment1Phoenix {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Course studentCourse;
+        List<Student> studentList;
+
+        studentList = new ArrayList<Student>();
+        studentList.add(new Student("Student1", "C0680", "Male", 3.53));
+        studentList.add(new Student("Student2", "C0681", "Male", 2.53));
+        studentList.add(new Student("Student3", "C0682", "Male", 3.04));
+        studentList.add(new Student("Student4", "C0683", "Female", 3.13));
+        studentList.add(new Student("Student5", "C0684", "Female", 2.23));
+        studentList.add(new Student("Student6", "C0685", "Male", 1.53));
+        studentList.add(new Student("Student6", "C0686", "Male", 3.52));
+        studentList.add(new Student("Student7", "C0686", "Male", 3.33));
+        studentCourse = new Course(studentList);
+        
+        System.out.println(studentCourse.getGradeMap());
     }
-    
+
 }
